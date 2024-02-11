ec0a20fc247e598331c6c1b0908e7b123fb9974f
Murilo Pereira <pereiramurilo540@gmail.com>
2024-02-11 19:15:41 -0300

    fist commit

---
+++ server.js
@@ -0,0 +1,50 @@
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./databe-memory.js";
+import { request } from "http";
+import { DatabasePostgress } from "./database-postgres.js";
+
+const server = fastify();
+const database = new DatabasePostgress;
+
+server.post("/videos", async(request, reply) => {
+  const { title, description, duration } = request.body;
+  await database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.get("/videos",async (request, reply) => {
+  const search = request.query.search;
+  const videos = await database.list(search);
+  return videos;
+});
+
+server.put("/videos/:id", async(request, reply) => {
+  const videoId = request.params.id;
+
+  const { title, description, duration } = request.body;
+
+  database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id",async (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return reply.status(204).send();
+});
+
+server.listen({
+  port: process.env.PORT ?? 3333
+});
